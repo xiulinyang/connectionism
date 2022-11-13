463ec81141856f8adfc4027b77d7e382a39ee442
Xiulin <81375342+xiulinyang@users.noreply.github.com>
2022-11-13 16:53:48 +0100

    Add files via upload

--- exp.sh
+++ exp.sh
@@ -1,21 +1,53 @@
-onmt_train -config data_eng1/run/phon/data_structure_eng1_irreg.yaml
-onmt_translate -model data_eng1/run/phon/model_step_1616.pt -src data_eng1/run/phon/src_train_irreg.txt -output data_eng1/run/phon/result_1616.txt -verbose
-rm data_eng1/run/phon/model_step_1616.pt
-onmt_translate -model data_eng1/run/phon/model_step_3232.pt -src data_eng1/run/phon/src_train_irreg.txt -output data_eng1/run/phon/result_3232.txt -verbose
-rm data_eng1/run/phon/model_step_3232.pt
-onmt_translate -model data_eng1/run/phon/model_step_4848.pt -src data_eng1/run/phon/src_train_irreg.txt -output data_eng1/run/phon/result_4848.txt -verbose
-rm data_eng1/run/phon/model_step_4848.pt
-onmt_translate -model data_eng1/run/phon/model_step_6464.pt -src data_eng1/run/phon/src_train_irreg.txt -output data_eng1/run/phon/result_6464.txt -verbose
-rm data_eng1/run/phon/model_step_6464.pt
-onmt_translate -model data_eng1/run/phon/model_step_8080.pt -src data_eng1/run/phon/src_train_irreg.txt -output data_eng1/run/phon/result_8080.txt -verbose
-rm data_eng1/run/phon/model_step_8080.pt
-onmt_translate -model data_eng1/run/phon/model_step_9696.pt -src data_eng1/run/phon/src_train_irreg.txt -output data_eng1/run/phon/result_9696.txt -verbose
-rm data_eng1/run/phon/model_step_9696.pt
-onmt_translate -model data_eng1/run/phon/model_step_11312.pt -src data_eng1/run/phon/src_train_irreg.txt -output data_eng1/run/phon/result_11312.txt -verbose
-rm data_eng1/run/phon/model_step_11312.pt
-onmt_translate -model data_eng1/run/phon/model_step_12928.pt -src data_eng1/run/phon/src_train_irreg.txt -output data_eng1/run/phon/result_12928.txt -verbose
-rm data_eng1/run/phon/model_step_12928.pt
-onmt_translate -model data_eng1/run/phon/model_step_14544.pt -src data_eng1/run/phon/src_train_irreg.txt -output data_eng1/run/phon/result_14544.txt -verbose
-rm data_eng1/run/phon/model_step_14544.pt
-onmt_translate -model data_eng1/run/phon/model_step_16155.pt -src data_eng1/run/phon/src_train_irreg.txt -output data_eng1/run/phon/result_16155.txt -verbose
-rm data_eng1/run/phon/model_step_16155.pt
+onmt_train -config data_english/run/phon/data_structure_eng_pho_irreg.yaml
+onmt_translate -model data_english/run/phon/model_step_976.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_976.txt -verbose
+rm data_english/run/phon/model_step_976.pt
+onmt_translate -model data_english/run/phon/model_step_1952.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_1952.txt -verbose
+rm data_english/run/phon/model_step_1952.pt
+onmt_translate -model data_english/run/phon/model_step_2928.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_2928.txt -verbose
+rm data_english/run/phon/model_step_2928.pt
+onmt_translate -model data_english/run/phon/model_step_3904.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_3904.txt -verbose
+rm data_english/run/phon/model_step_3904.pt
+onmt_translate -model data_english/run/phon/model_step_4880.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_4880.txt -verbose
+rm data_english/run/phon/model_step_4880.pt
+onmt_translate -model data_english/run/phon/model_step_5856.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_5856.txt -verbose
+rm data_english/run/phon/model_step_5856.pt
+onmt_translate -model data_english/run/phon/model_step_6832.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_6832.txt -verbose
+rm data_english/run/phon/model_step_6832.pt
+onmt_translate -model data_english/run/phon/model_step_7808.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_7808.txt -verbose
+rm data_english/run/phon/model_step_7808.pt
+onmt_translate -model data_english/run/phon/model_step_8784.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_8784.txt -verbose
+rm data_english/run/phon/model_step_8784.pt
+onmt_translate -model data_english/run/phon/model_step_9760.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_9760.txt -verbose
+rm data_english/run/phon/model_step_9760.pt
+onmt_translate -model data_english/run/phon/model_step_10736.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_10736.txt -verbose
+rm data_english/run/phon/model_step_10736.pt
+onmt_translate -model data_english/run/phon/model_step_11712.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_11712.txt -verbose
+rm data_english/run/phon/model_step_11712.pt
+onmt_translate -model data_english/run/phon/model_step_12688.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_12688.txt -verbose
+rm data_english/run/phon/model_step_12688.pt
+onmt_translate -model data_english/run/phon/model_step_13664.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_13664.txt -verbose
+rm data_english/run/phon/model_step_13664.pt
+onmt_translate -model data_english/run/phon/model_step_14640.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_14640.txt -verbose
+rm data_english/run/phon/model_step_14640.pt
+onmt_translate -model data_english/run/phon/model_step_15616.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_15616.txt -verbose
+rm data_english/run/phon/model_step_15616.pt
+onmt_translate -model data_english/run/phon/model_step_16592.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_16592.txt -verbose
+rm data_english/run/phon/model_step_16592.pt
+onmt_translate -model data_english/run/phon/model_step_17568.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_17568.txt -verbose
+rm data_english/run/phon/model_step_17568.pt
+onmt_translate -model data_english/run/phon/model_step_18544.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_18544.txt -verbose
+rm data_english/run/phon/model_step_18544.pt
+onmt_translate -model data_english/run/phon/model_step_19520.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_19520.txt -verbose
+rm data_english/run/phon/model_step_19520.pt
+onmt_translate -model data_english/run/phon/model_step_20496.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_20496.txt -verbose
+rm data_english/run/phon/model_step_20496.pt
+onmt_translate -model data_english/run/phon/model_step_21472.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_21472.txt -verbose
+rm data_english/run/phon/model_step_21472.pt
+onmt_translate -model data_english/run/phon/model_step_22448.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_22448.txt -verbose
+rm data_english/run/phon/model_step_22448.pt
+onmt_translate -model data_english/run/phon/model_step_23424.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_23424.txt -verbose
+rm data_english/run/phon/model_step_23424.pt
+onmt_translate -model data_english/run/phon/model_step_24400.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_24400.txt -verbose
+rm data_english/run/phon/model_step_24400.pt
+onmt_translate -model data_english/run/phon/model_step_24420.pt -src data_english/run/phon/src_train_irreg.txt -output data_english/run/phon/result_24420.txt -verbose
+rm data_english/run/phon/model_step_24420.pt
